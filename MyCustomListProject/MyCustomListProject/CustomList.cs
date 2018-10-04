32dd86ca31df499a7736e22bc82005e082fb9841
Traumatthew <43039716+Traumatthew@users.noreply.github.com>
2018-10-04 15:11:31 -0500

    Add function working

--- MyCustomListProject/MyCustomListProject/CustomList.cs
+++ MyCustomListProject/MyCustomListProject/CustomList.cs
@@ -8,5 +8,47 @@ namespace MyCustomListProject
 {
     public class CustomList<T>
     {
+        
+        private T[] items = new T[5]; //Array
+        private int count = 0;
+        public int Count { get { return count; } }
+        private int capacity = 4;
+
+        public void Add(T value)
+        {
+            if (items.Length > capacity)
+            {
+                T[] temp = new T[capacity * 2];
+                for (int i = 0; i < count; i++)
+                {
+                    temp[i] = items[i];
+                }
+                temp[count] = value;
+                count++;
+                items = temp;
+            }
+            else if (items.Length <= capacity)
+            {
+                items.count = value;
+            }
+            
+        }
+
+
+        public T this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+        private void IncreaseItemSize()
+        {
+            if (items)
+        }
+
+        public bool Remove()
+        {
+
+        }
     }
+   
 }
--- MyCustomListProject/UnitTestProject1/UnitTest1.cs
+++ MyCustomListProject/UnitTestProject1/UnitTest1.cs
@@ -7,9 +7,156 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_TestMethod1()
         {
+            //Arrange
             CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void Add_TestMethod2()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int countValue = 1;
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+            Assert.AreEqual(customList.Count, countValue);
+        }
+
+        [TestMethod]
+        public void Add_TestMethod3()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int value1 = 20;
+
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void Remove_TestMethod1()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int value1 = 1;
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Remove(value);
+
+            //Assert
+            Assert.AreEqual(value1, customList[0]);
+        }
+
+        [TestMethod]
+        public void Remove_TestMethod2()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int value1 = 39;
+            int countValue = 1;
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Remove(value);
+
+            //Assert
+            Assert.AreEqual(customList.Count, countValue);
+        }
+
+        [TestMethod]
+        public void Remove_TestMethod3()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int value1 = 20;
+
+
+            //Act
+            customList.Remove(value);
+            customList.Remove(value1);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void AdditionOverload_TwoStrings()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>(); {
+            CustomList<int> two = new CustomList<int>();
+            one.Add(1);
+            one.Add(2);
+            one.Add(3);
+            two.Add(4);
+            two.Add(5);
+            two.Add(6);
+
+            CustomList<int> expectedResult = new CustomList<int>();
+            expectedResult.Add(1);
+            expectedResult.Add(2);
+            expectedResult.Add(3);
+            expectedResult.Add(4);
+            expectedResult.Add(5);
+            expectedResult.Add(6);
+
+            //Act
+            CustomList<int> result = one + two;
+
+            //Assert
+            Assert.AreEqual(expectedResult[3], result[3]);
+        }
+        public void MinusOverload_TwoStrings()
+        {
+            CustomList<int> odd = new CustomList<int>();
+            CustomList<int> even = new CustomList<int>();
+            odd.Add(1);
+            odd.Add(3);
+            odd.Add(5);
+            odd.Add(7);
+
+            even.Add(2);
+            even.Add(4);
+            even.Add(6);
+            even.Add(8);
+
+            CustomList<int> expectedResult = new CustomList<int>();
+            expectedResult.Remove(1);
+            expectedResult.Remove(2);
+
+            //Act
+            CustomList<int> result = odd - even;
+
+            //Assert
+
+            Assert.AreEqual(expectedResult[3], result[]);
+
+
         }
     }
 }
